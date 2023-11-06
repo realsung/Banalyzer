34638753b039ba2fe50c2869fad333cd94ba4628
dpp0900 <_dpp@kakao.com>
2023-11-06 11:56:21 +0900

    Fsb detecter

--- scripts/가세혁/fsb.py
+++ scripts/가세혁/fsb.py
@@ -9,21 +9,35 @@ class _func:
         self.loc = hex(f)
         self.name = idc.get_func_name(f)
     def get_dec_func(self):
-        return idaapi.decompile(self.func)
+        return idaapi.decompile(self.func).__str__()
+    
     
 def log(func, msg):
-    print(f"[{func.hex}:{func.name}] {msg}")
+    print(f"[{func.loc}:{func.name}] {msg}")
 
-def check_printf(func):
+def check_printf(func) -> list:
+    ret = []
     log(func, "Checking printf")
-    dec_func = func.get_dec_func().split("\n")
+    dec_func = func.get_dec_func().split('\n')
     for line in range(len(dec_func)):
-        printf = re.findall(r'printf\(\"(.*)\"\)', dec_func[line])
-    print(printf)
+        arg = re.findall(r'printf\((\w+)\)', dec_func[line])
+        if arg:
+            arg = arg[0].split(',')
+            ret.append((line+1,arg))
+    return ret
+            
+def check_fsb(line, arg):
+    for a in arg:
+        if "%" in a:
+            return False
+    return True
 
 def main():
     for f in idautils.Functions():
         func = _func(f)
+        printf = check_printf(func)
+        for line, arg in printf:
+            if check_fsb(line, arg):
+                log(func, f"FSB found at line {line} with arg {arg}")
 
-
-print(main())
+main()
